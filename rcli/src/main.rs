9b726a69e350f55f57b71843c7f2225d56fe7ec4
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-04-05 12:17:13 +0200

    stub: add args

--- rcli/src/main.rs
+++ rcli/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
+
+/// RC4 file en/decryption
+#[derive(Parser, Debug)]
+struct Args {
+    /// Name of file to en/decrypt
+    #[clap(short, long, required=true, value_name = "FILE_NAME")]
+    file: String,
+
+    /// En/decryption key (hexadecimal bytes)
+    #[clap(
+        short,
+        long,
+        required = true,
+        value_name = "HEX_BYTES",
+        min_values = 5,
+        max_values = 256,
+    )]
+    key: Vec<String>,
+}
+
