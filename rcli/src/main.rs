504dd2c46b171b8512381f67d3062418ede45d31
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-04-12 19:33:30 +0200

    feat: cli functional

--- rcli/src/main.rs
+++ rcli/src/main.rs
@@ -1,8 +1,33 @@
 use clap::Parser;
+use rc4::Rc4;
+use std::fs::File;
+use std::io::prelude::{Read, Seek, Write};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    println!("{:?}", args);
+    let mut contents = Vec::new();
+
+    // Convert key string to byte array
+    let key_bytes: Vec<u8> = args
+        .key
+        .iter()
+        .map(|s| s.trim_start_matches("0x"))
+        .map(|s| u8::from_str_radix(s, 16).expect("Invalid hex key byte."))
+        .collect();
+
+    let mut file = File::options().read(true).write(true).open(&args.file)?;
+
+    file.read_to_end(&mut contents)?;
+
+    Rc4::apply_keystream_static(&key_bytes, &mut contents);
+
+    // Overwrite existing file
+    file.rewind()?;
+    file.write_all(&contents)?;
+
+    println!("Processed '{}'", args.file);
+
+    Ok(())
 }
 
 /// RC4 file en/decryption
